cc280189d5a2b5b3dda11134f85a16a36c4336b8
Anthony Ng <newyork.anthonyng@gmail.com>
2021-06-13 12:57:15 -0400

    use dotenv

--- src/googleSheet.js
+++ src/googleSheet.js
@@ -1,11 +1,14 @@
 const { GoogleSpreadsheet } = require("google-spreadsheet");
-const secret = require("../secret-key.json");
 
 const GOOGLE_SHEET_ID = `1pz6lCE9LMUJWkfwCdwxvzV2lLGjAM3DDJDIqZXi8Rz4`;
 
 async function getMainSheet() {
   const doc = new GoogleSpreadsheet(GOOGLE_SHEET_ID);
-  await doc.useServiceAccountAuth(secret);
+
+  await doc.useServiceAccountAuth({
+    client_email: process.env.GOOGLE_SHEET_EMAIL,
+    private_key: process.env.GOOGLE_SHEET_PRIVATE_KEY,
+  });
   await doc.loadInfo();
 
   const sheet = doc.sheetsByIndex[0];
